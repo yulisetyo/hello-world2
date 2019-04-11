ad00a732dd50f17ed1bd7677f11c4754cb8b2b12
yulisetyobudi <yuli.setyo@gmail.com>
2019-04-11 17:26:57 +0700

    perbaikan

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hello-world2';
+  isNavbarCollapsed;
+  closeMessage;
   data = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum';
   onChildChange(e){
     this.title = "Title "+e;
